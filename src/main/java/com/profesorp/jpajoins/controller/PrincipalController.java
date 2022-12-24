b1e57c5735ceef7b5674de8c9484216ee340cf19
cley9 <cleyutp@gmail.com>
2022-12-24 05:56:09 -0500

    V0.02 join sql server spring boot + lista de objeto sobre otro

--- src/main/java/com/profesorp/jpajoins/controller/PrincipalController.java
+++ src/main/java/com/profesorp/jpajoins/controller/PrincipalController.java
@@ -1,5 +1,6 @@
 package com.profesorp.jpajoins.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +17,26 @@ import com.profesorp.jpajoins.repository.invoiceHeaderRepository;
 public class PrincipalController {
 	@Autowired
 	invoiceHeaderRepository invoiceHeaderRepository;
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Invoiceheader> get(@PathVariable int id)
-	{
-		 Optional<Invoiceheader> invoice=invoiceHeaderRepository.findById(id);
-		 if (invoice.isPresent())
-			 return new ResponseEntity<>(invoice.get(),HttpStatus.OK);
-		 return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+
+	// @GetMapping("/data")
+	// // public ResponseEntity<Invoiceheader> get(@PathVariable int id)
+	// public ResponseEntity<Invoiceheader> get()
+	// {
+	// Optional<Invoiceheader> invoice=invoiceHeaderRepository.findAll();
+	// // Optional<Invoiceheader> invoice=invoiceHeaderRepository.findById(id);
+	// //List<Invoiceheader> invoice = invoiceHeaderRepository.findAll();
+	// if (invoice.isPresent())
+	// return new ResponseEntity<>(invoice.get(), HttpStatus.OK);
+	// return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	// }
+
+	@GetMapping("/data")
+	public ResponseEntity<List<Invoiceheader>> get() {
+		List<Invoiceheader> facturas = invoiceHeaderRepository.findAll();
+		if (facturas.isEmpty()) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(facturas, HttpStatus.OK);
 	}
+
 }
